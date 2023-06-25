24f70feae1e9fe05d24052e17774785ffb3577f6
CharalamposMoutsios <Charalampos.Moutsios@yh.nackademin.se>
2023-06-26 00:36:29 +0200

    document the TicketType.java

--- src/main/java/festivalbokaren/TicketType.java
+++ src/main/java/festivalbokaren/TicketType.java
@@ -1,8 +1,10 @@
 package festivalbokaren;
 
+//Enumeration that represent the different types of tickets.
+
 public enum TicketType {
-    VIP,
-    STANDARD,
-    STANDARD_PLUS,
-    OFFICIAL
+    VIP,             //VIP ticket type
+    STANDARD,        //Standard ticket type
+    STANDARD_PLUS,   //Standard Plus ticket type
+    OFFICIAL         //Official ticket type
 }
