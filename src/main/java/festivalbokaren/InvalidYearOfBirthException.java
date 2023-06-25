19d473b1e08c3a4c71c8f1e1888dec698b07d570
CharalamposMoutsios <Charalampos.Moutsios@yh.nackademin.se>
2023-06-26 00:41:30 +0200

    document the InvalidYearOfBirthException.java

--- src/main/java/festivalbokaren/InvalidYearOfBirthException.java
+++ src/main/java/festivalbokaren/InvalidYearOfBirthException.java
@@ -1,5 +1,8 @@
 package festivalbokaren;
-
+/*
+ * Custom exception class for invalid year of birth.
+ * This exception is thrown when an invalid yearofbirth is provided.
+ */
 public class InvalidYearOfBirthException extends Exception {
     public InvalidYearOfBirthException(String message) {
         super(message);
